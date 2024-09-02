a8f47035f6d595d9c34013fd7d987aa0e52449ee
qsoulior <74735618+qsoulior@users.noreply.github.com>
2024-09-02 22:08:49 +0300

    Implement develop/dev06

--- develop/dev06/task.go
+++ develop/dev06/task.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"bufio"
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
 /*
 === Утилита cut ===
 
@@ -13,6 +24,96 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+// Cut разбивает строку по delim и возвращает новую строку с полями, индексы которых указаны в fields.
+// Если separated равен true, то Cut возвращает false вторым значением для строки, которая не содержит delim.
+func Cut(s string, fields []int, delim string, separated bool) (string, bool) {
+	parts := strings.Split(s, delim) // части строки, полученные по delim
 
+	// Если строка не пустая и не содержит delim.
+	if len(parts) == 1 {
+		if separated {
+			return "", false
+		}
+		return parts[0], true
+	}
+
+	// Если строка содержит delim, и fields не пустой.
+	if len(fields) >= 1 {
+		newParts := make([]string, 0)
+		// Выбираем только те колонки, которые попали в fields.
+		for _, field := range fields {
+			if field < len(parts) {
+				newParts = append(newParts, parts[field])
+			}
+		}
+		// Обновляем части строки.
+		parts = newParts
+	}
+
+	// Соединяем части строки
+	return strings.Join(parts, delim), true
+}
+
+var (
+	fields    Fields
+	delimiter string
+	separated bool
+)
+
+// Тип полей для парсинга пакетом flag.
+type Fields []int
+
+// String возвращает строкове представление переменной типа Fields.
+func (f *Fields) String() string { return fmt.Sprintf("%v", *f) }
+
+// Set устанавливает значение переменной типа Fields из строки s.
+func (f *Fields) Set(s string) error {
+	// Делим строку s по разделителю "," и добавляем индексы полей в срез.
+	for _, value := range strings.Split(s, ",") {
+		field, err := strconv.Atoi(value)
+		if err != nil || field-1 < 0 {
+			return errors.New("fields are numbered from 1")
+		}
+		*f = append(*f, field-1)
+	}
+
+	// Сортируем индексы полей по возрастанию.
+	slices.Sort(*f)
+	return nil
+}
+
+// Errorf пишет данные в stderr.
+func Errorf(format string, a ...any) { fmt.Fprintf(os.Stderr, format, a...) }
+
+func init() {
+	// Настраиваем flag на парсинг параметров cut.
+	flag.Var(&fields, "f", "select only these fields; also print any line that contains no delimiter character, unless the -s option is specified")
+	flag.StringVar(&delimiter, "d", "\t", "use DELIM instead of TAB for field delimiter")
+	flag.BoolVar(&separated, "s", false, "do not print lines not containing delimiters")
+}
+
+func main() {
+	flag.Parse()
+
+	// Проверяем наличие флага -f.
+	if len(fields) == 0 {
+		Errorf("you must specify fields\n")
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
+
+	// Сканируем строки и для каждой из них возвращаем результат операции.
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		result, ok := Cut(scanner.Text(), fields, delimiter, separated)
+		if ok {
+			fmt.Println(result)
+		}
+	}
+
+	// Возвращаем ошибку, если не удалось отсканировать строки.
+	if err := scanner.Err(); err != nil {
+		Errorf("failed to read file: %s\n", err)
+		os.Exit(1)
+	}
 }
--- develop/dev06/task_test.go
+++ develop/dev06/task_test.go
@@ -1,1 +1,67 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCut(t *testing.T) {
+	type args struct {
+		fields    []int
+		delim     string
+		separated bool
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  string
+		want1 bool
+	}{
+		{"-f 2,4 -d ':'", args{[]int{1, 3}, ":", false}, " white: frost", true},
+		{"-f 5 -d ':'", args{[]int{4}, ":", false}, "", true},
+		{"-f 1 -d ','", args{[]int{1}, ",", false}, "Winter: white: snow: frost", true},
+		{"-f 1 -d ',' -s", args{[]int{1}, ",", true}, "", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := Cut("Winter: white: snow: frost", tt.args.fields, tt.args.delim, tt.args.separated)
+			if got != tt.want {
+				t.Errorf("Cut() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("Cut() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
+
+func TestFields_String(t *testing.T) {
+	f := Fields{0, 1}
+	want := "[0 1]"
+
+	if got := f.String(); !reflect.DeepEqual(got, want) {
+		t.Errorf("Fields.String() = %v, want %v", got, want)
+	}
+}
+
+func TestFields_Set(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name    string
+		f       Fields
+		args    args
+		wantErr bool
+	}{
+		{"ValidString", Fields{}, args{"1,2"}, false},
+		{"InvalidString", Fields{}, args{"0,1"}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.f.Set(tt.args.s); (err != nil) != tt.wantErr {
+				t.Errorf("Fields.Set() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
