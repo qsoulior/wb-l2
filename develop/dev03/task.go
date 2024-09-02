f8317c0c71a5371ccb835e38871611e05c36bf1e
qsoulior <74735618+qsoulior@users.noreply.github.com>
2024-09-02 13:45:58 +0300

    Implement develop/dev03

--- develop/dev03/task.go
+++ develop/dev03/task.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"slices"
+	"strconv"
+	"strings"
+)
+
 /*
 === Утилита sort ===
 
@@ -25,6 +37,243 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+// Структура опциональных параметров сортировки.
+type SortOptions struct {
+	key     int  // номер колонки
+	numeric bool // сортировка по целочисленному значению
+	reverse bool // сортировка в обратном порядке
+	unique  bool // не выводить повторяющиеся строки
 
+	month        bool // сортировка по названию месяца
+	ignoreBlanks bool // игнорировать ведущие и хвостовые пробельные символы
+	check        bool // проверка, отсортированы ли строки
+	humanNumeric bool // сортировка по целочисленному значению с суффиксом
+}
+
+// Тип функции сравнения двух строк.
+type CompareFunc func(a, b string) int
+
+// Шаблон для парсинга месяца в строке.
+const MonthLayout = "Jan"
+
+// ParseNumeric извлекает целочисленное значение из строки s.
+func ParseNumeric(s string) int {
+	re := regexp.MustCompile(`\d+`)
+	value := re.FindString(s)
+	d, _ := strconv.Atoi(value)
+	return d
+}
+
+// CompareNumeric возвращает результат сравнения двух строк,
+// содержащих целочисленные значения.
+func CompareNumeric(a, b string) int {
+	return ParseNumeric(a) - ParseNumeric(b)
+}
+
+// ParseNumeric извлекает целочисленное значение с определенным суффиксом из строки s.
+func ParseNumericWithSuffix(s string) int {
+	multipliers := map[string]int{"K": 1e3, "M": 1e6, "G": 1e9, "T": 1e12}
+
+	re := regexp.MustCompile(`(\d+)([KMGT])`)
+	values := re.FindStringSubmatch(s)
+	if len(values) < 3 {
+		return 0
+	}
+
+	multiplier, _ := multipliers[values[2]]
+	d, _ := strconv.Atoi(values[1])
+	return d * multiplier
+}
+
+// CompareNumericWithSuffix возвращает результат сравнения двух строк,
+// содержащих целочисленные значения с определенными суффиксами.
+func CompareNumericWithSuffix(a, b string) int {
+	return ParseNumericWithSuffix(a) - ParseNumericWithSuffix(b)
+}
+
+// ParseMonth извлекает порядковый номер месяца из строки s.
+func ParseMonth(s string) int {
+	months := map[string]int{"jan": 0, "feb": 1, "mar": 2, "apr": 3, "may": 4, "jun": 5, "jul": 6, "aug": 7, "sep": 8, "oct": 9, "nov": 10, "dec": 11}
+	re := regexp.MustCompile(`jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec`)
+
+	key := re.FindString(strings.ToLower(s))
+	month, _ := months[key]
+	return month
+}
+
+// CompareMonth возвращает результат сравнения двух строк,
+// содержащих строковые представления месяцов.
+func CompareMonth(a, b string) int {
+	return ParseMonth(a) - ParseMonth(b)
+}
+
+// CompareKey возвращает функцию сравнения cmp для колонки с номером key.
+// Колонка - часть строки, отделенная пробельными символами.
+func CompareKey(cmp CompareFunc, key int) CompareFunc {
+	return func(a, b string) int {
+		colsA := strings.Fields(a)
+		colsB := strings.Fields(b)
+		if i := key - 1; i < len(colsA) && i < len(colsB) {
+			return cmp(colsA[i], colsB[i])
+		}
+		return 0
+	}
+}
+
+// Compare возвращает результат сравнения двух строк
+// в зависимости от опциональных параметров.
+func Compare(opts SortOptions) CompareFunc {
+	var cmp CompareFunc
+
+	switch {
+	case opts.numeric:
+		cmp = CompareNumeric
+	case opts.humanNumeric:
+		cmp = CompareNumericWithSuffix
+	case opts.month:
+		cmp = CompareMonth
+	default:
+		cmp = strings.Compare
+	}
+
+	if opts.key > 0 {
+		return CompareKey(cmp, opts.key)
+	}
+
+	return cmp
+}
+
+// IgnoreBlanks преобразует исходный срез так,
+// чтобы строки не содержали пробельные символы в начале и конце.
+func IgnoreBlanks(in []string) {
+	for i, str := range in {
+		in[i] = strings.TrimSpace(str)
+	}
+}
+
+// IsSorted возвращает результат проверки на то, отсортирован ли срез
+// в порядке, определенном опциональными параметрами.
+func IsSorted(in []string, opts SortOptions) bool {
+	// Переворачиваем срез, если надо проверить сортировку в обратном порядке.
+	if opts.reverse {
+		slices.Reverse(in)
+	}
+
+	// Проверяем, отсортирован ли срез строк.
+	isSorted := slices.IsSortedFunc(in, Compare(opts))
+
+	// Проверяем, соблюдена ли строгая сортировка.
+	if opts.unique {
+		// Сортировка считается не строгой, если есть равные строки.
+		return isSorted && len(in) == len(slices.Compact(in))
+	}
+
+	return isSorted
+}
+
+// Sort преобразует исходный срез так, чтобы строки были расположены
+// в порядке, определенном опциональными параметрами.
+func Sort(in *[]string, opts SortOptions) {
+	strs := *in
+
+	// Сортируем срез строк.
+	slices.SortFunc(strs, Compare(opts))
+
+	// Переворачиваем срез, если надо отсортировать в обратном порядке.
+	if opts.reverse {
+		slices.Reverse(strs)
+	}
+
+	// Удаляем дубликаты строк из среза.
+	if opts.unique {
+		*in = slices.Compact(strs)
+	}
+}
+
+// Errorf пишет данные в stderr.
+func Errorf(format string, a ...any) { fmt.Fprintf(os.Stderr, format, a...) }
+
+// ReadStrings читает строки из r и записывает их в срез строк.
+func ReadStrings(r io.Reader) ([]string, error) {
+	strs := make([]string, 0)
+	scanner := bufio.NewScanner(r)
+
+	for scanner.Scan() {
+		strs = append(strs, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return strs, nil
+}
+
+// Переменные под параметры сортировки.
+var (
+	key                                                                int
+	numeric, reverse, unique, month, ignoreBlanks, check, humanNumeric bool
+)
+
+func init() {
+	// Настраиваем flag на парсинг параметров сортировки.
+	flag.IntVar(&key, "k", 0, "sort via a key")
+	flag.BoolVar(&numeric, "n", false, "compare according to string numerical value")
+	flag.BoolVar(&reverse, "r", false, "reverse the result of comparisons")
+	flag.BoolVar(&unique, "u", false, "with -c, check for strict ordering; without -c, output only the first of an equal run")
+	flag.BoolVar(&month, "M", false, "compare (unknown) < 'JAN' < ... < 'DEC'")
+	flag.BoolVar(&ignoreBlanks, "b", false, "ignore leading blanks")
+	flag.BoolVar(&check, "c", false, "check for sorted input; do not sort")
+	flag.BoolVar(&humanNumeric, "h", false, "compare human readable numbers (e.g., 2K 1G)")
+}
+
+func main() {
+	// Парсим аргументы параметров сортировки.
+	flag.Parse()
+	opts := SortOptions{key, numeric, reverse, unique, month, ignoreBlanks, check, humanNumeric}
+
+	// Парсим путь до файла из аргумента.
+	path := flag.Arg(0)
+	if path == "" {
+		Errorf("empty file path")
+		return
+	}
+
+	// Открываем файл на чтение.
+	file, err := os.Open(path)
+	if err != nil {
+		Errorf("failed to open file: %s", err)
+		return
+	}
+
+	defer func() {
+		if err := file.Close(); err != nil {
+			Errorf("failed to close file: %s", err)
+		}
+	}()
+
+	// Читаем строки из файла и записываем в срез строк.
+	strs, err := ReadStrings(file)
+	if err != nil {
+		Errorf("failed to read file: %s", err)
+		return
+	}
+
+	// Удаляем пробельные символы из строк.
+	if opts.ignoreBlanks {
+		IgnoreBlanks(strs)
+	}
+
+	// Проверяем, отсортированы ли строки.
+	if opts.check {
+		sorted := IsSorted(strs, opts)
+		fmt.Println(sorted)
+		return
+	}
+
+	// Сортируем строки и выводим отсортированный результат.
+	Sort(&strs, opts)
+	for _, str := range strs {
+		fmt.Println(str)
+	}
 }
--- develop/dev03/task_test.go
+++ develop/dev03/task_test.go
@@ -1,1 +1,245 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestParseNumeric(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"InvalidString", args{"abcabc"}, 0},
+		{"ValidString", args{"abc10abc"}, 10},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ParseNumeric(tt.args.s); got != tt.want {
+				t.Errorf("ParseNumeric() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCompareNumeric(t *testing.T) {
+	type args struct {
+		a string
+		b string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"InvalidStrings", args{"abcabc", "cbacba"}, 0},
+		{"ValidStrings", args{"abc11abc", "cba10cba"}, 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CompareNumeric(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("CompareNumeric() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestParseNumericWithSuffix(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"InvalidString", args{"abcabc"}, 0},
+		{"ValidString", args{"abc10Kabc"}, 10000},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ParseNumericWithSuffix(tt.args.s); got != tt.want {
+				t.Errorf("ParseNumericWithSuffix() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCompareNumericWithSuffix(t *testing.T) {
+	type args struct {
+		a string
+		b string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"InvalidStrings", args{"abcabc", "cbacba"}, 0},
+		{"ValidStrings", args{"abc1Mabc", "cba400Kcba"}, 6e5},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CompareNumericWithSuffix(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("CompareNumericWithSuffix() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestParseMonth(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"InvalidString", args{"abcabc"}, 0},
+		{"ValidString", args{"abcJUNabc"}, 5},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ParseMonth(tt.args.s); got != tt.want {
+				t.Errorf("ParseMonth() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCompareMonth(t *testing.T) {
+	type args struct {
+		a string
+		b string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"InvalidStrings", args{"abcabc", "cbacba"}, 0},
+		{"ValidStrings", args{"abcJUNabc", "cbaFEBcba"}, 4},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CompareMonth(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("CompareMonth() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCompare(t *testing.T) {
+	type args struct {
+		opts SortOptions
+	}
+	tests := []struct {
+		name string
+		args args
+		want CompareFunc
+	}{
+		{"Compare", args{SortOptions{}}, strings.Compare},
+		{"CompareNumeric", args{SortOptions{numeric: true}}, CompareNumeric},
+		{"CompareHumanNumeric", args{SortOptions{humanNumeric: true}}, CompareNumericWithSuffix},
+		{"CompareMonth", args{SortOptions{month: true}}, CompareMonth},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Compare(tt.args.opts); fmt.Sprintf("%p", got) != fmt.Sprintf("%p", tt.want) {
+				t.Errorf("Compare() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestIgnoreBlanks(t *testing.T) {
+	in := []string{"   abc   ", " bcd", "def "}
+	IgnoreBlanks(in)
+	out := []string{"abc", "bcd", "def"}
+
+	if !reflect.DeepEqual(in, out) {
+		t.Errorf("got = %v, want %v", in, out)
+	}
+}
+
+func TestIsSorted(t *testing.T) {
+	type args struct {
+		in   []string
+		opts SortOptions
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"Sorted", args{[]string{"abc", "bca", "bca"}, SortOptions{}}, true},
+		{"Unsorted", args{[]string{"bca", "abc", "bca"}, SortOptions{}}, false},
+		{"SortedReverse", args{[]string{"bca", "abc", "abc"}, SortOptions{reverse: true}}, true},
+		{"SortedUnique", args{[]string{"abc", "bca"}, SortOptions{unique: true}}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsSorted(tt.args.in, tt.args.opts); got != tt.want {
+				t.Errorf("IsSorted() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSort(t *testing.T) {
+	type args struct {
+		in   []string
+		opts SortOptions
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{"Sort", args{[]string{"bca", "abc", "bca"}, SortOptions{}}, []string{"abc", "bca", "bca"}},
+		{"SortReverse", args{[]string{"bca", "abc", "bca"}, SortOptions{reverse: true}}, []string{"bca", "bca", "abc"}},
+		{"SortUnique", args{[]string{"bca", "abc", "bca"}, SortOptions{unique: true}}, []string{"abc", "bca"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			Sort(&tt.args.in, tt.args.opts)
+			if !reflect.DeepEqual(tt.args.in, tt.want) {
+				t.Errorf("got = %v, want %v", tt.args.in, tt.want)
+			}
+		})
+	}
+}
+
+func TestReadStrings(t *testing.T) {
+	type args struct {
+		r io.Reader
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []string
+		wantErr bool
+	}{
+		{"ValidReader", args{strings.NewReader("abc\nbca")}, []string{"abc", "bca"}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ReadStrings(tt.args.r)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ReadStrings() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ReadStrings() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
