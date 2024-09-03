ed0b09c74cfb764baeff74a8c86766dddefbcf8f
qsoulior <74735618+qsoulior@users.noreply.github.com>
2024-09-03 23:17:33 +0300

    Implement develop/dev08

--- develop/dev08/task.go
+++ develop/dev08/task.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+
+	"github.com/mitchellh/go-ps"
+)
+
 /*
 === Взаимодействие с ОС ===
 
@@ -20,6 +31,186 @@ package main
 и при необходимости выводит результат на экран. Интерактивный сеанс поддерживается до тех пор, пока не будет введена команда выхода (например \quit).
 */
 
-func main() {
+type Command interface {
+	Execute(args ...string) (string, error)
+}
 
+type CD struct {
+}
+
+func (c CD) Execute(args ...string) (string, error) {
+	if len(args) > 0 {
+		dir := args[0]
+		err := os.Chdir(dir)
+		if err != nil {
+			return "", fmt.Errorf("failed to cd %s: %w", dir, err)
+		}
+	}
+	return "", nil
+}
+
+type PWD struct {
+}
+
+func (c PWD) Execute(args ...string) (string, error) {
+	wd, err := os.Getwd()
+	if err != nil {
+		return "", fmt.Errorf("failed to pwd: %w", err)
+	}
+
+	return wd, nil
+}
+
+type Echo struct {
+}
+
+func (c Echo) Execute(args ...string) (string, error) {
+	var builder strings.Builder
+	for i, arg := range args {
+		builder.WriteString(arg)
+		if i < len(args)-1 {
+			builder.WriteRune(' ')
+		}
+	}
+	return builder.String(), nil
+}
+
+type Kill struct {
+}
+
+func (c Kill) Execute(args ...string) (string, error) {
+	if len(args) == 0 {
+		return "", nil
+	}
+	pid, err := strconv.Atoi(args[0])
+	if err != nil {
+		return "", fmt.Errorf("failed to get pid %d: %w", pid, err)
+	}
+	process, err := os.FindProcess(pid)
+	if err != nil {
+		return "", fmt.Errorf("failed to find process %d: %w", pid, err)
+	}
+	err = process.Kill()
+	if err != nil {
+		return "", fmt.Errorf("failed to kill processs %d: %w", pid, err)
+	}
+	return fmt.Sprintf("process %d was killed", pid), nil
+}
+
+type PS struct {
+}
+
+func (c PS) Execute(args ...string) (string, error) {
+	var limit int
+	if len(args) > 0 {
+		limit, _ = strconv.Atoi(args[0])
+	}
+
+	processes, err := ps.Processes()
+	if err != nil {
+		return "", fmt.Errorf("failed to list processes: %w", err)
+	}
+
+	if limit <= 0 || limit > len(processes) {
+		limit = len(processes)
+	}
+
+	var builder strings.Builder
+	builder.WriteString("PID\tPPID\tExecutable\n")
+	for i := 0; i < limit; i++ {
+		builder.WriteString(fmt.Sprintf("%d\t%d\t%s", processes[i].Pid(), processes[i].PPid(), processes[i].Executable()))
+		if i < limit-1 {
+			builder.WriteRune('\n')
+		}
+	}
+	return builder.String(), nil
+}
+
+type Exec struct {
+}
+
+func (c Exec) Execute(args ...string) (string, error) {
+	if len(args) == 0 {
+		return "", nil
+	}
+	name := args[0]
+	cmd := exec.Command(name, args[1:]...)
+	output, err := cmd.Output()
+	if err != nil {
+		return "", fmt.Errorf("failed to exec: %w", err)
+	}
+	return string(output), nil
+}
+
+var commands = map[string]Command{
+	"cd":   new(CD),
+	"pwd":  new(PWD),
+	"echo": new(Echo),
+	"kill": new(Kill),
+	"ps":   new(PS),
+	"exec": new(Exec),
+}
+
+func ParseLine(line string) (Command, []string, error) {
+	args := strings.Split(strings.TrimSpace(line), " ")
+	cmd, ok := commands[args[0]]
+	if !ok {
+		return nil, nil, fmt.Errorf("command not found: %s", args[0])
+	}
+	return cmd, args[1:], nil
+}
+
+func ExecuteLines(lines []string) (string, error) {
+	if len(lines) == 0 {
+		return "", nil
+	}
+
+	cmd, args, err := ParseLine(lines[0])
+	if err != nil {
+		return "", err
+	}
+
+	result, err := cmd.Execute(args...)
+	if err != nil {
+		return "", err
+	}
+
+	for i := 1; i < len(lines); i++ {
+		cmd, _, err := ParseLine(lines[i])
+		if err != nil {
+			return "", err
+		}
+
+		result, err = cmd.Execute(result)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	return result, nil
+}
+
+func main() {
+	fmt.Fprint(os.Stdout, "Welcome to shell! Enter \\q or \\quit to exit.\n> ")
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		text := scanner.Text()
+		if text == "\\quit" || text == "\\q" {
+			break
+		}
+
+		if text == "" {
+			fmt.Fprint(os.Stdout, "> ")
+			continue
+		}
+
+		lines := strings.Split(text, "|")
+		result, err := ExecuteLines(lines)
+		if err != nil {
+			fmt.Fprint(os.Stderr, err)
+		} else {
+			fmt.Fprint(os.Stdout, result)
+		}
+		fmt.Fprint(os.Stdout, "\n> ")
+	}
 }
--- develop/dev08/task_test.go
+++ develop/dev08/task_test.go
@@ -1,1 +1,201 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCD_Execute(t *testing.T) {
+	c := new(CD)
+	type args struct {
+		args []string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{"ValidArgument", args{nil}, false},
+		{"InvalidArgument", args{[]string{""}}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := c.Execute(tt.args.args...)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CD.Execute() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
+
+func TestPWD_Execute(t *testing.T) {
+	c := new(PWD)
+	_, err := c.Execute()
+	if (err != nil) != false {
+		t.Errorf("PWD.Execute() error = %v, wantErr %v", err, false)
+		return
+	}
+}
+
+func TestEcho_Execute(t *testing.T) {
+	c := new(Echo)
+	got, _ := c.Execute("abc", "bcd")
+	want := "abc bcd"
+	if got != want {
+		t.Errorf("Echo.Execute() = %v, want %v", got, want)
+	}
+}
+
+func TestKill_Execute(t *testing.T) {
+	c := new(Kill)
+	type args struct {
+		args []string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{"EmptyArgument", args{}, "", false},
+		{"InvalidArgument", args{[]string{""}}, "", true},
+		{"InvalidArgumentPID", args{[]string{"-1"}}, "", true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := c.Execute(tt.args.args...)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Kill.Execute() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Kill.Execute() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPS_Execute(t *testing.T) {
+	c := new(PS)
+	type args struct {
+		args []string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{"EmptyArgument", args{}, false},
+		{"ValidArgument", args{[]string{"10"}}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := c.Execute(tt.args.args...)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("PS.Execute() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
+
+func TestExec_Execute(t *testing.T) {
+	c := new(Exec)
+	type args struct {
+		args []string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{"EmptyArgument", args{}, false},
+		{"InvalidArgument", args{[]string{""}}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := c.Execute(tt.args.args...)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Exec.Execute() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
+
+func TestParseLine(t *testing.T) {
+	type args struct {
+		line string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    Command
+		want1   []string
+		wantErr bool
+	}{
+		{"ValidLine", args{"echo 123"}, new(Echo), []string{"123"}, false},
+		{"InvalidLine", args{""}, nil, nil, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1, err := ParseLine(tt.args.line)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ParseLine() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ParseLine() got = %v, want %v", got, tt.want)
+			}
+			if !reflect.DeepEqual(got1, tt.want1) {
+				t.Errorf("ParseLine() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
+
+func TestExecuteLines(t *testing.T) {
+	type args struct {
+		lines []string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{"NoLines", args{nil}, "", false},
+		{"ValidFirstLine", args{[]string{"echo 123"}}, "123", false},
+		{"InvalidFirstLine", args{[]string{""}}, "", true},
+		{"InvalidFirstLineArgument", args{[]string{"cd \\0"}}, "", true},
+		{"ValidLines", args{[]string{"echo 123", "echo"}}, "123", false},
+		{"InvalidLines", args{[]string{"echo 123", ""}}, "", true},
+		{"InvalidLinesArguments", args{[]string{"echo \\0", "cd"}}, "", true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ExecuteLines(tt.args.lines)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ExecuteLines() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("ExecuteLines() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_main(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			main()
+		})
+	}
+}
