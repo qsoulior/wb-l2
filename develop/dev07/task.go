6d89d05017bcf0476fcb97995bc56451e6e15078
qsoulior <74735618+qsoulior@users.noreply.github.com>
2024-09-03 14:32:16 +0300

    Implement develop/dev07

--- develop/dev07/task.go
+++ develop/dev07/task.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 /*
 === Or channel ===
 
@@ -33,6 +38,55 @@ start := time.Now()
 fmt.Printf(“fone after %v”, time.Since(start))
 */
 
-func main() {
+// or объединяет один или более done-каналов в single-канал, который закроется,
+// если хотя бы один из его составляющих каналов закроется.
+func or(channels ...<-chan any) <-chan any {
+	// Условия для выхода из рекурсии.
+	switch len(channels) {
+	case 0:
+		return nil
+	case 1:
+		return channels[0]
+	}
 
+	// Создаем single-канал done, который будет закрыт при закрытии составляющих каналов.
+	done := make(chan any)
+
+	// Если каналов >= 2, то создаем горутину с селектом.
+	// Она будет читать каналы с индексами 0 и 1, а также or для каналов с индексами >= 2.
+	go func() {
+		defer close(done)
+		select {
+		case <-channels[0]:
+		case <-channels[1]:
+		case <-or(append(channels[2:], done)...):
+			// Рекурсивно вызываем or для каналов с индексами >= 2.
+			// Добавляем в аргументы done-канал, чтобы вызовы завершались при его закрытии.
+		}
+	}()
+
+	// Возвращаем single-канал done.
+	return done
+}
+
+func main() {
+	sig := func(after time.Duration) <-chan any {
+		c := make(chan any)
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	start := time.Now()
+	<-or(
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(1*time.Second),
+		sig(1*time.Hour),
+		sig(1*time.Minute),
+	)
+
+	fmt.Printf("done after %v\n", time.Since(start))
 }
--- develop/dev07/task_test.go
+++ develop/dev07/task_test.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"testing"
+	"time"
+)
+
+func Test_or(t *testing.T) {
+	sig := func(after time.Duration) <-chan any {
+		c := make(chan any)
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	t.Run("NoChannels", func(t *testing.T) {
+		if got := or(); got != nil {
+			t.Errorf("or() = %v, want %v", got, nil)
+		}
+	})
+
+	tests := []struct {
+		name     string
+		channels []<-chan any
+	}{
+		{"FirstChannel", []<-chan any{sig(0), sig(1 * time.Minute)}},
+		{"SecondChannel", []<-chan any{sig(1 * time.Minute), sig(0)}},
+		{"ThirdChannel", []<-chan any{sig(1 * time.Minute), sig(1 * time.Minute), sig(0)}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if _, got := <-or(tt.channels...); got {
+				t.Errorf("<-or() = %v, want %v", got, false)
+			}
+		})
+	}
+}
